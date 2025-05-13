64ac9fd73a47b852e15d0680b91431e94e3ffbb4
Jaz <62318169+jasrayatwal@users.noreply.github.com>
2025-05-13 00:23:07 -0700

    Fix eslint errors

--- frontend/src/components/NewSpot/NewSpot.jsx
+++ frontend/src/components/NewSpot/NewSpot.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { makeNewSpot } from '../../store/spots';
+//import { useState } from 'react';
+//import { useDispatch } from 'react-redux';
+//import { makeNewSpot } from '../../store/spots';
 import './NewSpot.css'
 
 const NewSpot = () => {
